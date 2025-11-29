f1e8d4a24c85b651241c51b4a6ab1d8ec29371ff
vuchint sutapalli <vuchint.sutapalli@gmail.com>
2025-11-30 04:38:49 +0530

    bug fixes

--- TurnBasedGames/src/components/Boards/TicTacToeBoard.jsx
+++ TurnBasedGames/src/components/Boards/TicTacToeBoard.jsx
@@ -8,18 +8,19 @@ const PLAYER_TO_MARK_MAP = {
 
 export default function TicTacToeBoard({ board, onCellClick }) {
 	return (
-		<div className="grid grid-cols-3 gap-2 w-full max-w-xl aspect-square bg-slate-700 p-2 rounded-lg">
+		<div className="grid grid-cols-3 gap-2 min-w-xs md:min-w-md max-w-xl aspect-square bg-slate-700 p-2 rounded-lg">
 			{board.map((player, index) => {
 				const mark = PLAYER_TO_MARK_MAP[player]; // 'X', 'O', or undefined
 				return (
 					<button
 						key={index}
 						onClick={() => onCellClick(index)}
-						className="bg-slate-800 rounded-lg flex items-center justify-center text-6xl md:text-8xl font-bold text-white hover:bg-slate-600 disabled:hover:bg-slate-800"
+						className="bg-slate-800 rounded-lg flex items-center justify-center text-lg md:text-4xl font-bold text-white hover:bg-slate-600 disabled:hover:bg-slate-800"
 						disabled={!!player} // Button is disabled if the cell is occupied by any player
 					>
 						{mark === "X" && <span className="text-sky-400">{mark}</span>}
 						{mark === "O" && <span className="text-amber-400">{mark}</span>}
+						{!mark && <span>&nbsp;</span>}
 					</button>
 				);
 			})}
